72c6168f9990a355b5213ca8076f39336a6af9fd
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-03 19:05:18 +0000

    Added answer

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ est, xvals, yvals, = 1, np.zeros(20), np.zeros(20)
 for i in range(0,20) :
   xvals[i] = i+1
   # Your code goes here
+  est = 1 + 1. / ( 1 + est )
+  yvals[i]=est
 
 plt.plot( xvals, yvals, 'ko')
+plt.xlabel("Iteration")
+plt.ylabel("Estimate")
 plt.savefig( "roottwo.png")
